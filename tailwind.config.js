33677f3a50e81d81592c87b8c781058279c60f29
Asad Kamran <kamranasad7@gmail.com>
2024-02-25 16:12:41 +0500

    tailwind project colors and root css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          DEFAULT: '#0d0d0d',
+          50: '#f6f6f6',
+          100: '#e7e7e7',
+          200: '#d1d1d1',
+          300: '#b0b0b0',
+          400: '#888888',
+          500: '#6d6d6d',
+          600: '#5d5d5d',
+          700: '#4f4f4f',
+          800: '#454545',
+          900: '#3d3d3d',
+          950: '#0d0d0d',
+          medium: '#252525',
+          light: '#cccccc'
+        },
+        'secondary': '#fe4a23',
+        'tertiary': '#f8d250'
+      }
+    },
   },
   plugins: [],
 }
